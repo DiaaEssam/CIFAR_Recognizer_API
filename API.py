40049d9b025a7f62579892a676739763f2804d51
Diaa Essam <102432512+DiaaEssam@users.noreply.github.com>
2023-07-26 07:41:37 +0300

    Add files via upload

--- API.py
+++ API.py
@@ -10,10 +10,32 @@ app=Flask(__name__) # it's a common step to start with this
 Swagger(app) # pass the App to Swagger
 
 current_directory = os.path.abspath(os.path.dirname(__file__))
-model_path = os.path.join(current_directory, "model")
+model_path = os.path.join(current_directory, "model.h5")
 
 classifier=tf.keras.models.load_model(model_path)
 
+classes = {
+    0:'aquatic mammals',
+    1:'fish',
+    2:'flowers',
+    3:'food containers',
+    4:'fruit and vegetables',
+    5:'household electrical devices',
+    6:'household furniture',
+    7:'insects',
+    8:'large carnivores',
+    9:'large man-made outdoor things',
+    10:'large natural outdoor scenes',
+    11:'large omnivores and herbivores',
+    12:'medium-sized mammals',
+    13:'non-insect invertebrates',
+    14:'people',
+    15:'reptiles',
+    16:'small mammals',
+    17:'trees',
+    18:'vehicles 1',
+    19:'vehicles 2',
+}
 @app.route('/') # must be written to define the root page or main page to display
 # this will display a web page having welcome all in it
 def welcome():
@@ -42,9 +64,10 @@ def predict_A_sample():
     image = Image.open(image)
     image = np.array(image.resize((224, 224)))
     image = image.reshape((1, 224, 224,3))
+    print(image.shape)
 
     prediction=np.argmax(tf.nn.softmax(classifier.predict(image)[0]))
-    return "The digit is: " + str(prediction)
+    return "The digit is: " + classes[prediction]
 
 # a page for predicting csv file, can be used through Postman
 @app.route('/predict_file',methods=["POST"])
